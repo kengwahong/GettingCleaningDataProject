4d9566cbef072887d13eab3db4e44c1123707e99
Ong Keng Wah <kerropi@singnet.com.sg>
2014-10-25 17:02:00 +0800

    Commit README.md and run_analysis.R
    Submitted the README and R Script files.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 GettingCleaningDataProject
 ==========================
+
+### Introduction
+The purpose of this project is to collect, work with, and clean a data set. 
+The goal is to prepare tidy data that can be used for later analysis. 
+
+One of the most exciting areas in all of data science right now is wearable computing.
+Ref Article: http://www.insideactivitytracking.com/data-science-activity-tracking-and-the-battle-for-the-worlds-top-sports-brand/
+
+Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. 
+The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. 
+A full description is available at the site where the data was obtained:
+	http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+Data for the project:
+	https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+### Assignment
+
+To create one R script called run_analysis.R that does the following. 
+	1. Merges the training and the test sets to create one data set.
+	2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+	3. Uses descriptive activity names to name the activities in the data set
+	4. Appropriately labels the data set with descriptive variable names. 
+	5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+### The Sciprt: run_analysis.R
+
+Step 1: Load 'features_info.txt'. The File captures the information about the variables used on the feature vector.
+Step 2: Load 'activity_labels.txt'. The File captures the the class labels with their activity name.
+Step 3: Rename the Columns (activity labels) to desciptive activity names
+Step 4: Load TEST data files. Each feature vector is a row on the text file. 
+	Extracts only the measurements on the mean and standard deviation for each measurement.
+	- 'test/X_test.txt': Test set.
+	- 'test/y_test.txt': Test labels.
+	- 'test/subject_test.txt": List of test subjects
+Step 5: Load TRAIN data files. Each feature vector is a row on the text file. 
+	Extracts only the measurements on the mean and standard deviation for each measurement.
+	- 'train/X_train.txt': Training set.
+	- 'train/y_train.txt': Training labels.
+	- 'train/subject_train.txt": List of training subjects
+Step 6: Merges the training and the test sets to create one data set.
+Step 7: Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+Step 8: Write output into a file
---
+++ run_analysis.R
@@ -0,0 +1,72 @@
+# USAGE: source("run_analysis.R")
+library(plyr)
+library(reshape2)
+
+# Read 'features_info.txt'
+# File captures the information about the variables used on the feature vector.
+print("Reading FEATURE file...")
+flush.console()
+feature <- read.table("./features.txt", header = FALSE, col.names=c("id","name"))
+reqCol <- grepl("mean|std", feature$name) 
+
+# Read 'activity_labels.txt'
+# File captures the the class labels with their activity name.
+print("Reading ACTIVITY file...")
+flush.console()
+activity <- read.table("./activity_labels.txt", header = FALSE, col.names=c("id","Activity"))
+
+# Rename Columns
+feature$name <- gsub("tBody", "Time.Body", feature$name)
+feature$name <- gsub("tGravity", "Time.Gravity", feature$name)
+feature$name <- gsub("fBody", "FFT.Body", feature$name)
+feature$name <- gsub("fGravity", "FFT.Gravity", feature$name)
+feature$name <- gsub("\\-mean\\(\\)\\-", ".Mean.", feature$name)
+feature$name <- gsub("\\-std\\(\\)\\-", ".Std.", feature$name)
+feature$name <- gsub("\\-mean\\(\\)", ".Mean", feature$name)
+feature$name <- gsub("\\-std\\(\\)", ".Std", feature$name)
+
+# Read TEST file
+# - 'test/X_test.txt': Test set.
+# - 'test/y_test.txt': Test labels.
+# - 'test/subject_test.txt": List of test subjects
+# - Each feature vector is a row on the text file.
+print("Reading TEST files...")
+flush.console()
+valTest <- read.table("./test/X_test.txt", header = FALSE, col.names=feature[,2]) ## REQ 4.Appropriately labels the data set with descriptive variable names.
+subTest <- read.table("./test/subject_test.txt", header = FALSE, col.names=c("Subject"))
+idTest <- read.table("./test/y_test.txt", header = FALSE, col.names=c("Activity.ID"))
+dataTest <- cbind(subTest, idTest, valTest) #Bind to a single data set, with Subject, Activity and Features
+dataTest <- dataTest[,reqCol] ## REQ 2: Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+# Read TRAIN file
+# - 'train/X_train.txt': Training set.
+# - 'train/y_train.txt': Training labels.
+# - 'train/subject_train.txt": List of training subjects
+# - Each feature vector is a row on the text file.
+print("Reading TRAIN files...")
+flush.console()
+valTrain <- read.table("./train/X_train.txt", header = FALSE, col.names=feature[,2]) ## REQ 4: Appropriately labels the data set with descriptive variable names. 
+subTrain <- read.table("./train/subject_train.txt", header = FALSE, col.names=c("Subject"))
+idTrain <- read.table("./train/y_train.txt", header = FALSE, col.names=c("Activity.ID"))
+dataTrain <- cbind(subTrain, idTrain, valTrain)
+dataTrain <- dataTrain[,reqCol] ## REQ 2: Extracts only the measurements on the mean and standard deviation for each measurement.
+
+# Merge TEST and TRAIN data
+## REQ 1: Merges the training and the test sets to create one data set.
+tidy <- rbind(dataTest, dataTrain)
+
+## REQ 3: Uses descriptive activity names to name the activities in the data set
+tidy <- merge(tidy, activity, by.x="Activity.ID", by.y="id")
+
+## REQ 5: Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+idLabels   <- c("Subject", "Activity.ID", "Activity")
+dataLabels <- setdiff(colnames(tidy), idLabels)
+meltData <- melt(tidy, id = idLabels, measure.vars = dataLabels)
+# Apply mean function to dataset using dcast function
+tidyMean <- dcast(meltData, Subject + Activity.ID + Activity ~ variable, mean)
+
+# Write output into a file
+print("Writing Tidy + Mean file...")
+flush.console()
+write.table(tidyMean, file="./tidy_mean.txt", sep="\t", row.names=FALSE)
+write.table(tidy, file="./tidy.txt", sep="\t", row.names=FALSE)
